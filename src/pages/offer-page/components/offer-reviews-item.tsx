51c236c1892661e443deba81f281b5306954030c
SapGri <sapgri360@gmail.com>
2024-11-30 22:52:19 +0300

    добавляет пакет dayjs в проект

--- src/pages/offer-page/components/offer-reviews-item.tsx
+++ src/pages/offer-page/components/offer-reviews-item.tsx
@@ -1,20 +1,6 @@
 import Rating from '../../../components/rating/rating';
+import dayjs from 'dayjs';
 import { Response } from '../../../types';
-
-const monthes: string[] = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
 
 export default function OfferReviewsItem({
   user: {name, avatarUrl},
@@ -39,7 +25,7 @@ export default function OfferReviewsItem({
         <Rating prefix='reviews' rating={rating} />
         <p className="reviews__text">{comment}</p>
         <time className="reviews__time" dateTime="2019-04-24">
-          {`${monthes[new Date(date).getMonth()]} ${new Date(date).getFullYear()}`}
+          {`${dayjs(date).format('MMMM YYYY')}`}
         </time>
       </div>
     </li>
