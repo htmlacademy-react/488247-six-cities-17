dd2b4e27f18f8ba1f68d2c2610231667419bd012
SapGri <sapgri360@gmail.com>
2024-12-01 14:25:12 +0300

    feat: добавляет маршрутизацию в приложение

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,31 @@
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
 
 export default function App() {
   return (
-    <MainPage />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main} element={<MainPage />}
+        />
+        <Route
+          path={AppRoute.Login} element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Favorites} element={<FavoritesPage />}
+        />
+        <Route
+          path={AppRoute.Offer} element={<OfferPage />}
+        />
+        <Route
+          path={AppRoute.NotFoundPage} element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
