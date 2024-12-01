339121313bb3d9b0ea402fb3d49d1793d4d9eefd
SapGri <sapgri360@gmail.com>
2024-12-01 14:27:17 +0300

    feat: добавляет компонент scroll-to-top

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,12 +3,14 @@ import LoginPage from '../../pages/login-page/login-page';
 import FavoritesPage from '../../pages/favorites-page/favorites-page';
 import OfferPage from '../../pages/offer-page/offer-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import ScrollToTop from '../scroll-to-top/scroll-to-top';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppRoute } from '../../const';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route
           path={AppRoute.Main} element={<MainPage />}
